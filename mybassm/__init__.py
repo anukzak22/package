8165e7008c4757af8d6359f28b90352865e8604e
anukzak <anahit_zakaryan@edu.aua.am>
2023-05-17 14:37:53 +0400

    the end

--- mybassm/__init__.py
+++ mybassm/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Anahit Zakaryan"""
 __email__ = 'anukzak@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 from mybassm.mybassm import (diffusion, bass_f, bass_F, predict_bass_model)
